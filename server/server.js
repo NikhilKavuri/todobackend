0912d66c4108823fa44c58c0ac7ce4c16c843505
Nikhil Kavuri <nikhilkavuri1503@gmail.com>
2023-04-27 01:40:44 +0530

    B

--- server/server.js
+++ server/server.js
@@ -1,58 +1,69 @@
-const http = require("http");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs').promises;
 
-const server = http.createServer((req, res) => {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+const app = express();
+const port = 3000;
 
-  // Handle requests
-  if (req.method === "OPTIONS") {
-    res.writeHead(204);
-    return res.end();
-  }
-  if (req.method === "GET" && req.url === "/todos") {
-    const todos = JSON.parse(fs.readFileSync("./todos.json"));
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify(todos));
-  }
-  if (req.method === "POST" && req.url === "/todo/new") {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    req.on("end", () => {
-      const todos = JSON.parse(fs.readFileSync("./todos.json"));
-      const newNote = {
-        id: todos.length + 1,
-        text: JSON.parse(body).text,
-        complete: false,
-      };
-      const updated = [...todos, newNote];
-      fs.writeFileSync("./todos.json", JSON.stringify(updated));
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(newNote));
-    });
-  }
-  if (req.method === "DELETE" && req.url.startsWith("/todo/delete/")) {
-    const lastTerm = parseInt(req.url.split("/")[3]);
-    console.log(lastTerm);
-    const todos = JSON.parse(fs.readFileSync("./todos.json"));
-    const todoIndex = todos.findIndex((todo) => todo.id === lastTerm);
-    if (todoIndex >= 0) {
-      const deletedTodo = todos.splice(todoIndex, 1)[0];
-      fs.writeFileSync("./todos.json", JSON.stringify(todos));
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(200);
-      res.end(JSON.stringify(deletedTodo));
-    } else {
-      res.writeHead(404);
-      res.end();
-    }
-  }
-  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await fs.readFile('./todos.json');
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(todos);
+  } catch(err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.post('/todo/new', async (req, res) => {
+  try {
+    const todos = await fs.readFile('./todos.json');
+    const newNote = {
+      id: todos.length + 1,
+      text: req.body.text,
+      complete: false,
+    };
+    const updated = [...todos, newNote];
+    await fs.writeFile('./todos.json', JSON.stringify(updated));
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(newNote);
+  } catch(err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const todos = await fs.readFile('./todos.json');
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
+    if (todoIndex >= 0) {
+      const deletedTodo = todos.splice(todoIndex, 1)[0];
+      await fs.writeFile('./todos.json', JSON.stringify(todos));
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(deletedTodo);
+    } else {
+      res.status(404).send('Todo not found');
+    }
+  } catch(err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+  
 server.listen(5000, () => console.log("Server is running on port 5000"));
