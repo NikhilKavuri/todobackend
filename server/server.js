2b4408756dc682b10dd48989a74e0abf92a07937
Nikhil Kavuri <nikhilkavuri1503@gmail.com>
2023-04-27 09:57:58 +0530

    ab

--- server/server.js
+++ server/server.js
@@ -23,47 +23,5 @@ app.get('/todos', async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
-
-app.post('/todo/new', async (req, res) => {
-  try {
-    const todos = await fs.readFile('./todos.json');
-    const newNote = {
-      id: todos.length + 1,
-      text: req.body.text,
-      complete: false,
-    };
-    const updated = [...todos, newNote];
-    await fs.writeFile('./todos.json', JSON.stringify(updated));
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(newNote);
-  } catch(err) {
-    console.error(err);
-    res.status(500).send('Internal server error');
-  }
-});
-
-app.delete('/todo/delete/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const todos = await fs.readFile('./todos.json');
-    const todoIndex = todos.findIndex((todo) => todo.id === id);
-    if (todoIndex >= 0) {
-      const deletedTodo = todos.splice(todoIndex, 1)[0];
-      await fs.writeFile('./todos.json', JSON.stringify(todos));
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).send(deletedTodo);
-    } else {
-      res.status(404).send('Todo not found');
-    }
-  } catch(err) {
-    console.error(err);
-    res.status(500).send('Internal server error');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
-
   
 server.listen(5000, () => console.log("Server is running on port 5000"));
